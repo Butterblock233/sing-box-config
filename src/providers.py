ded7adb27a1a2b88e7a3e1fa73d85732397faf6b
Butterblock233 <Voltage15312@outlook.com>
2024-11-06 16:47:26 +0800

    removed sensetive data

--- src/providers.py
+++ src/providers.py
@@ -1,9 +1,7 @@
-import json
 import json5
 import group
-provider_link="https://flyairport.trafficmanager.net/dee495db53a5db97a/link/3IaxvaiExLc7oaDN?singbox=1"
 
-def update_providers(profile_link,profile_path, config_path,output_path):
+def update_providers(profile_path, config_path,output_path):
 	"""Downloads a provider profile from a given link, and updates a config.jsonc with the outbounds from the downloaded provider profile
 	
 	Args:
@@ -11,7 +9,6 @@ def update_providers(profile_link,profile_path, config_path,output_path):
 		profile_path (str): Path to save the downloaded provider profile
 		config_path (str): Path to the config.jsonc to update
 	"""
-	#subprocess.run(["curl", "-o",profile_path,profile_link])
 	with open(profile_path, "r",encoding='utf-8') as file:
 		profile_data = json5.load(file)
 	with open(config_path, "r",encoding='utf-8') as file:
@@ -23,10 +20,15 @@ def update_providers(profile_link,profile_path, config_path,output_path):
 	config_data['outbounds'][0]['outbounds'] += ["日本"]
 	config_data['outbounds'][0]['outbounds'] += ["美国"]
 
+    #Update outbound "proxy"
+	config_data['outbounds'] +=[{"type": "selector",
+							  "tag": "PROXY",
+							  "outbounds": ["节点选择"]
+							  }]
 	# Append groups
-	config_data["outbounds"] += [group.update_groups(config_data,"日本",type="selector")]
-	config_data["outbounds"] += [group.update_groups(config_data,"美国")]
-
+	config_data["outbounds"] += [group.update_groups(config_data,"日本")]
+	config_data["outbounds"] += [group.update_groups(config_data,"美国",type="selector")]
+	# config_data["route"]["final"]="direct"
 	#Write updated config to config.jsonc
 	with open(output_path,'w',encoding='utf-8') as file:
 		json5.dump(config_data, file, 
@@ -35,4 +37,4 @@ def update_providers(profile_link,profile_path, config_path,output_path):
 			 quote_keys=True,
 			 trailing_commas=False,
 			 separators=(',', ': '))
-update_providers(provider_link,"profile.json","config.jsonc","../config.jsonc")
+update_providers("profile.json","config.jsonc","../config.jsonc")
